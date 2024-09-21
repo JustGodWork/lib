48823752f078d3a5cebfb145c137478686675c75
JustGod <85418813+JustGodWork@users.noreply.github.com>
2024-09-21 16:43:30 +0200

    Removed database call in index file

--- lib/index.lua
+++ lib/index.lua
@@ -4,7 +4,6 @@ require 'lib.convar.index';
 require 'lib.color.index';
 --require 'lib.discord.index'; # Disabled for now due to issues with discord-js and FiveM NodeJS
 require 'lib.events.index';
-require 'lib.database.index';
 require 'lib.game.index';
 require 'lib.entity.index';
 require 'lib.ui.index';
