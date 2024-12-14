9fdac8919714b2455f3d28bbadcfb8d2644b3c0e
Nishi <james.renaud.75@gmail.com>
2024-12-14 09:24:30 +0100

    update index

--- lib/ui/index.lua
+++ lib/ui/index.lua
@@ -39,7 +39,6 @@ if (not lib.is_server) then
 
     require 'lib.ui.menu.items.UIButton';
     require 'lib.ui.menu.items.UICheckBox';
-    require 'lib.ui.menu.items.UIInfo';
     require 'lib.ui.menu.items.UILine';
     require 'lib.ui.menu.items.UIList';
     require 'lib.ui.menu.items.UISeparator';
